0f27149f7a9fd96fa2c7dc2fab9ecd0a2c54024e
Nahyun <from.owol@gmail.com>
2025-12-31 16:44:30 +0900

    refactor: exam 페이지 공통 레이아웃 적용

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -3,9 +3,11 @@ import { ReactNode } from 'react'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-dvh flex flex-col">
+    <div className="h-screen flex flex-col">
       <Header />
-      <main className="flex-1  w-full mx-auto max-w-6xl px-4 py-6">{children}</main>
+      <main className="flex-1 overflow-y-auto w-full mx-auto max-w-6xl px-4 py-6">
+        {children}
+      </main>
     </div>
   )
 }
