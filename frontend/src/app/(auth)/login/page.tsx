3fa252b472d6966dd1225372f1e6d5940b5c6301
tjdwns99 <beans2853@gmail.com>
2025-12-31 17:45:50 +0900

    style: 로그인 페이지 스타일 수정

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,21 +1,22 @@
 import LoginForm from '@/components/login/LoginForm'
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Login() {
   return (
-    <main className="min-h-screen flex flex-col justify-center">
-      <section className="flex flex-col gap-2 items-center mx-auto py-4 px-8">
-        {/* TODO: 로고 삽입 */}
-        <h1 className="mb-10">오프너(로고)</h1>
+    <div className="min-h-screen flex flex-col justify-center">
+      <main className="min-w-sm flex flex-col gap-4 items-center mx-auto py-4 px-8">
+        <div className="relative w-full h-10 mb-10">
+          <Image src={'/image/logo_h56_p.svg'} alt="오프너" fill className="object-contain" />
+        </div>
         <LoginForm />
-        {/* TODO: 회원가입 링크 설정 */}
         <span className="text-sm">
           계정이 없으신가요?{' '}
-          <Link href={'/'} className="underline">
+          <Link href={'/register'} className="underline">
             회원가입
           </Link>
         </span>
-      </section>
-    </main>
+      </main>
+    </div>
   )
 }
