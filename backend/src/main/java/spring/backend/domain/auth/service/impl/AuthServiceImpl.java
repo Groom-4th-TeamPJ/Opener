341e93d5ea88474663acabecf852100fdb3deffb
mskim <msung98@naver.com>
2026-01-02 10:09:28 +0900

    feat: [BE] [G2-63] 인증 서비스 초기 설정

--- backend/src/main/java/spring/backend/domain/auth/service/impl/AuthServiceImpl.java
+++ backend/src/main/java/spring/backend/domain/auth/service/impl/AuthServiceImpl.java
@@ -1,4 +1,34 @@
 package spring.backend.domain.auth.service.impl;
 
-public class AuthSercieimpl {
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.backend.domain.auth.dto.request.FormSignupRequest;
+import spring.backend.domain.auth.dto.response.TokenResponse;
+import spring.backend.domain.auth.service.spec.AuthService;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AuthServiceImpl implements AuthService {
+
+  @Override
+  public TokenResponse formSignup(FormSignupRequest req) {
+    return null;
+  }
+
+  @Override
+  public TokenResponse oauthSignup(OAuthSignupRequest req) {
+    return null;
+  }
+
+  @Override
+  public void logout(String accessToken, String refreshToken) {
+
+  }
+
+  @Override
+  public TokenResponse refreshToken(String refreshToken) {
+    return null;
+  }
 }
---
+++ backend/src/main/java/spring/backend/domain/auth/service/impl/CustomUserDetailService.java
@@ -0,0 +1,30 @@
+package spring.backend.domain.auth.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import spring.backend.domain.user.model.entity.Credentials;
+import spring.backend.domain.user.repository.spec.UserCredentialRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+  private final UserCredentialRepository userCredentialRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String email) {
+
+    Credentials credential = userCredentialRepository
+            .findUserCredentialByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+
+    return org.springframework.security.core.userdetails.User
+            .withUsername(credential.getEmail())
+            .password(credential.getPassword())
+            .roles("USER")
+            .build();
+  }
+}
