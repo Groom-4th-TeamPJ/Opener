2eec98641ce6161dd94ba3385cf15fb9bc50b0d9
tjdwns99 <beans2853@gmail.com>
2026-01-02 15:16:49 +0900

    feat:[FE][G2-114] 로그인 UI 구성
    * feat: 로그인 UI 구성
    
    * feat: 로그인 UI 수정
    
    * style: 로그인 페이지 스타일 수정
    
    * style: 로그인 폼 스타일 수정
    
    * chore: import 경로 수정
    
    * style: 플레이스홀더 삽입
    
    * refactor: 폼 타입 정의

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,22 @@
+import LoginForm from '@/components/login/LoginForm'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Login() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center">
+      <main className="min-w-sm flex flex-col gap-4 items-center mx-auto py-4 px-8">
+        <div className="relative w-full h-10 mb-10">
+          <Image src={'/image/logo_h56_p.svg'} alt="오프너" fill className="object-contain" />
+        </div>
+        <LoginForm />
+        <span className="text-sm">
+          계정이 없으신가요?{' '}
+          <Link href={'/register'} className="underline">
+            회원가입
+          </Link>
+        </span>
+      </main>
+    </div>
+  )
+}
---
+++ frontend/src/components/login/LoginForm.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { useForm } from 'react-hook-form'
+import LoginFormView from '@/components/login/LoginFormView'
+
+export type LoginFormValues = {
+  email: string
+  password: string
+}
+
+const ERROR_MSG: string =
+  '아이디 또는 비밀번호가 잘못되었습니다.\n아이디와 비밀번호를 정확히 입력해주세요.'
+
+export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    clearErrors,
+    setError,
+    resetField,
+  } = useForm<LoginFormValues>({ defaultValues: { email: '', password: '' } })
+
+  const onSubmit = async (form: LoginFormValues) => {
+    clearErrors()
+    if (!form.email || !form.password) {
+      setError('root', { message: ERROR_MSG })
+      return
+    }
+    try {
+      // TODO: 추후 API 연동
+    } catch {
+      resetField('password')
+      setError('root', { message: ERROR_MSG })
+    }
+  }
+
+  const handleOAuth = () => {
+    // TODO: OAuth 처리
+  }
+
+  return (
+    <LoginFormView
+      register={register}
+      onSubmit={handleSubmit(onSubmit)}
+      errors={errors}
+      isSubmitting={isSubmitting}
+      onOAuthClick={handleOAuth}
+    />
+  )
+}
