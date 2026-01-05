e0c4e06cb1502e50ba2c9de0b0a9bbd62a459517
Nahyun <from.owol@gmail.com>
2026-01-05 17:33:42 +0900

    feat: Popover 컴포넌트 수정 및 InfoTooltip 컴포넌트 구현

---
+++ frontend/src/components/common/Popover.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import * as React from 'react'
+import * as PopoverPrimitive from '@radix-ui/react-popover'
+import cn from '@/utils/cn'
+
+function Popover({ ...props }: React.ComponentProps<typeof PopoverPrimitive.Root>) {
+  return <PopoverPrimitive.Root data-slot="popover" {...props} />
+}
+
+function PopoverTrigger({ ...props }: React.ComponentProps<typeof PopoverPrimitive.Trigger>) {
+  return <PopoverPrimitive.Trigger data-slot="popover-trigger" {...props} />
+}
+
+function PopoverContent({
+  className,
+  align = 'center',
+  sideOffset = 4,
+  arrowClassName, // 1. 화살표 전용 클래스를 받을 수 있게 추가
+  children, // 2. children을 명시적으로 분리
+  ...props
+}: React.ComponentProps<typeof PopoverPrimitive.Content> & {
+  arrowClassName?: string
+}) {
+  return (
+    <PopoverPrimitive.Portal>
+      <PopoverPrimitive.Content
+        data-slot="popover-content"
+        align={align}
+        sideOffset={sideOffset}
+        className={cn(
+          'bg-white text-text-primary data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 origin-(--radix-popover-content-transform-origin) rounded-md border p-4 shadow-md outline-hidden',
+          className
+        )}
+        {...props}
+      >
+        {children}
+        {/* 3. 화살표 컴포넌트 추가 */}
+        <PopoverPrimitive.Arrow
+          className={cn('fill-popover', arrowClassName)}
+          width={12}
+          height={6}
+        />
+      </PopoverPrimitive.Content>
+    </PopoverPrimitive.Portal>
+  )
+}
+
+function PopoverAnchor({ ...props }: React.ComponentProps<typeof PopoverPrimitive.Anchor>) {
+  return <PopoverPrimitive.Anchor data-slot="popover-anchor" {...props} />
+}
+
+export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor }
