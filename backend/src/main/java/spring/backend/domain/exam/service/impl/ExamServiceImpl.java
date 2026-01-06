6ff99eed812cdebdd26bff228352949adce8996a
JunwonLee <jununida@gmail.com>
2026-01-06 18:10:54 +0900

    feat: [BE] [G2-148] 문제풀이 조회 Service 추가

---
+++ backend/src/main/java/spring/backend/domain/exam/service/impl/ExamServiceImpl.java
@@ -0,0 +1,28 @@
+package spring.backend.domain.exam.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.backend.domain.exam.dto.response.ExamResponse;
+import spring.backend.domain.exam.model.enums.Category;
+import spring.backend.domain.exam.model.enums.ExamType;
+import spring.backend.domain.exam.repository.spec.ExamRepository;
+import spring.backend.domain.exam.service.spec.ExamService;
+
+import java.util.Optional;
+
+@Service
+public class ExamServiceImpl implements ExamService {
+
+    private final ExamRepository examRepository;
+
+    public ExamServiceImpl(ExamRepository examRepository) {
+        this.examRepository = examRepository;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<ExamResponse> findExamWithQuestions(Integer examYear, ExamType examType, Category category) {
+
+        return examRepository.findExamWithQuestions(examYear, examType, category);
+    }
+}
--- backend/src/main/java/spring/backend/domain/exam/service/impl/ExamlServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package spring.backend.domain.exam.service.impl;
-
-public class ExamlServiceImpl {
-}
