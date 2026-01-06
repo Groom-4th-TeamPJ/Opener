262e39d30d6d0259ad85b9168413f06a5d0888ab
JunwonLee <jununida@gmail.com>
2026-01-06 18:05:29 +0900

    feat: [BE] [G2-148] response에 code, name으로 나오게 수정

--- backend/src/main/java/spring/backend/domain/exam/model/enums/ExamType.java
+++ backend/src/main/java/spring/backend/domain/exam/model/enums/ExamType.java
@@ -1,5 +1,10 @@
 package spring.backend.domain.exam.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ExamType {
 
     M06("6월 모의고사"),
@@ -12,7 +17,28 @@ public enum ExamType {
         this.name = name;
     }
 
+    public String getCode() {
+        return name();
+    }
+
     public String getName() {
         return name;
+    }
+
+    // JSON 역직렬화 ({"code":"CSAT","name":"수학능력시험"} 등 처리)
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public static ExamType fromJson(@JsonProperty("code") String code,
+                                    @JsonProperty("name") String name) {
+        if (code != null) {
+            try {
+                return ExamType.valueOf(code);
+            } catch (IllegalArgumentException ignored) { }
+        }
+        if (name != null) {
+            for (ExamType e : values()) {
+                if (e.getName().equals(name)) return e;
+            }
+        }
+        throw new IllegalArgumentException("Unknown ExamType: code=" + code + ", name=" + name);
     }
 }
