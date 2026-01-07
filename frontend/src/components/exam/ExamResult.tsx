8d77cda370b9fef1132ed274cf7748f3dfc36ea2
Choijiin007 <cji2025@naver.com>
2026-01-07 09:19:21 +0900

    feat:[FE][G2-132] 문제 풀이 종료 UI 구성
    * feat: Shadcn - Toast 설치
    
    * feat:[FE] 변형 문제 UI 구성 및 global.css 애니메이션 추가
    
    * feat:[FE] 타입 및 목데이터 분리, 코드 수정
    
    * fix: lint 오류 수정
    
    * comment:AnalysisSection.tsx  주석 추가
    
    * feat:[FE] 문제 풀이 종료 UI 구성
    
    * feat:[FE] 임시 아이콘 파일 추가, 학습 결과 컴포넌트 분리
    
    * refactor: 학습 결과 요약(ExamResultSummary)컴포넌트 분리
    
    * fix:삭제되어야 하는 파일 삭제 및 충돌 해결
    
    * fix:충돌 해결
    
    * refactor: w- 값 변경
    
    * refactor:[FE] ResultData 타입 파일 삭제 및 이동
    
    * Rename:component/icons 폴더 내로 아이콘 이동
    
    * fix:충돌 해결
    
    * fix:[FE] 충돌 해결 (ResultData 추가)
    
    * refactor:[FE] 공백 추가
    
    ---------
    
    Co-authored-by: cji <gugugeul273@gmail.com>

---
+++ frontend/src/components/exam/ExamResult.tsx
@@ -0,0 +1,35 @@
+'use client'
+import Button from '@/components/common/Button'
+import { Modal, ModalContent, ModalFooter, ModalHeader } from '@/components/common/Modal'
+import { useRouter } from 'next/navigation'
+import ExamResultSummary from './ExamResultSummary'
+
+export default function ExamResult() {
+  const router = useRouter()
+
+  const onClose = () => {
+    router.back()
+  }
+
+  const handleGoToDashboard = () => {
+    router.push('/')
+  }
+  return (
+    <Modal open={true} onClose={onClose} className="md:max-w-97.5 lg:max-w-150">
+      <ModalHeader closable={true} onClose={onClose} className="flex">
+        <div className="flex justify-between">
+          <h2 className="font-bold text-lg">학습 결과</h2>
+        </div>
+      </ModalHeader>
+
+      <ModalContent className="flex flex-col gap-4">
+        <ExamResultSummary />
+      </ModalContent>
+      <ModalFooter>
+        <Button className="flex-1 h-12" onClick={handleGoToDashboard}>
+          대시보드로 이동하기
+        </Button>
+      </ModalFooter>
+    </Modal>
+  )
+}
--- frontend/src/types/exam.ts
+++ frontend/src/types/exam.ts
@@ -47,3 +47,10 @@ export interface ChatMessage {
   timestamp: string
   highlight?: string
 }
+
+export type ResultData = {
+  solveTime: string
+  correctCount: number
+  wrongCount: number
+  openerCount: number
+}
