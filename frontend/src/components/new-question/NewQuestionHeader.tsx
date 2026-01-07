59d5cd010ebf5fe864cb52f8fba2856c9e603636
cji <gugugeul273@gmail.com>
2026-01-07 14:50:56 +0900

    feat:[FE] <Stopwatch/> 컴포넌트 추가

--- frontend/src/components/new-question/NewQuestionHeader.tsx
+++ frontend/src/components/new-question/NewQuestionHeader.tsx
@@ -1,14 +1,11 @@
-import { Timer } from 'lucide-react'
+import Stopwatch from '../shared/Stopwatch'
 
 export default function NewQuestionHeader() {
   return (
     <div>
       <div className="flex justify-between">
-        <h2 className="font-bold text-lg">변형 문제</h2>
-        <div className="flex gap-1">
-          <Timer width={24} height={24} className="stroke-Neutral-600" />
-          <span>{/* 스탑워치 구현 예정*/}</span>
-        </div>
+        <h2 className="font-bold md:text-lg lg:text-xl">변형 문제</h2>
+        <Stopwatch />
       </div>
     </div>
   )
