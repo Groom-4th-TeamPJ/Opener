f11f3eaa2f0abc49dee1b408ce9899a716a0bb7b
cji <gugugeul273@gmail.com>
2026-01-07 14:37:25 +0900

    Remove:[FE] 폴더명 변경

--- frontend/src/components/NewQuestionModal/NewQuestionExam.tsx
+++
@@ -1,5 +0,0 @@
-import { NewQuestionExamProps } from '@/types/exam-variant'
-
-export default function NewQuestionExam({ passage }: NewQuestionExamProps) {
-  return <div className="md:pb-8 lg:pb-16">{passage}</div>
-}
