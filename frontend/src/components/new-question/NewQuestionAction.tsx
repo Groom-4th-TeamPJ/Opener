2165b9b4cff0f6ab3e21ed59c5df37dc988b3c67
Choijiin007 <cji2025@naver.com>
2026-01-08 17:25:05 +0900

    feat:[FE][G2-152]변형 문제 분기 처리(단답식 UI 추가)
    * feat: Shadcn - Toast 설치
    
    * feat:[FE] 변형 문제 UI 구성 및 global.css 애니메이션 추가
    
    * feat:[FE] 타입 및 목데이터 분리, 코드 수정
    
    * fix: lint 오류 수정
    
    * comment:AnalysisSection.tsx  주석 추가
    
    * feat:[FE] 문제 풀이 종료 UI 구성
    
    * feat:[FE] 임시 아이콘 파일 추가, 학습 결과 컴포넌트 분리
    
    * refactor: 학습 결과 요약(ExamResultSummary)컴포넌트 분리
    
    * fix:삭제되어야 하는 파일 삭제 및 충돌 해결
    
    * fix:충돌 해결
    
    * refactor: w- 값 변경
    
    * feat:[FE] 단답식  분기 처리
    
    * Remove:[FE] 중복된 아이콘 파일 삭제
    
    * Remove:[FE] 사용하지 않는 타입 파일 삭제
    
    * fix:[FE]lint 오류 해결
    
    * refactor:[FE] 경로에 @ 추가
    
    * Remove:[FE] 폴더명 변경
    
    * refactor:[FE] 코드 리뷰 반영하여 조건문 수정
    
    * refactor:[FE] 선택지 디자인 변경 및 폴더명 변경 저장
    
    * feat:[FE] <Stopwatch/> 컴포넌트 추가
    
    * style:[FE] input 스타일 적용
    
    * feat:[FE] 단답식 답안 결과 컴포넌트로 분리
    
    * feat:[FE] 숫자만 입력 가능한 정규식 추가, 디자인 반영하여 수정
    
    * refactor:[FE] 경로에 @ 추가
    
    * refactor:<FRQAnswer> 공통 컴포넌트 변경
    
    * feat:[FE] 답안 선택용 radio input 컴포넌트 분리
    
    * fix:[FE] 아이콘 오류로 인해 새로 불러옴
    
    * refactor:[FE] <ResultAnswer/>로 컴포넌트로 객관식 선택지 불러오기
    
    ---------
    
    Co-authored-by: cji <gugugeul273@gmail.com>

--- frontend/src/components/new-question/NewQuestionAction.tsx
+++ frontend/src/components/new-question/NewQuestionAction.tsx
@@ -2,19 +2,22 @@ import type { NewQuestionActionProps } from '@/types/exam-variant'
 import Button from '@/components/common/Button'
 
 export default function NewQuestionAction({
+  type,
   isSubmitted,
   selected,
+  frqAnswer,
   onSubmit,
   onBack,
 }: NewQuestionActionProps) {
+  const isValueEmpty = type === 'MCQ' ? selected == null : frqAnswer == null
   return (
     <>
       {!isSubmitted ? (
-        <Button className="flex-1 h-[48]" disabled={selected === null} onClick={onSubmit}>
+        <Button className="flex-1 h-12" disabled={isValueEmpty} onClick={onSubmit}>
           제출하기
         </Button>
       ) : (
-        <Button className="flex-1 h-[48]" onClick={onBack}>
+        <Button className="flex-1 h-12" onClick={onBack}>
           원래 문제로 돌아가기
         </Button>
       )}
--- frontend/src/components/new-question/NewQuestionModal.tsx
+++ frontend/src/components/new-question/NewQuestionModal.tsx
@@ -4,7 +4,7 @@ import { Modal, ModalContent, ModalFooter, ModalHeader } from '@/components/comm
 import { useRouter } from 'next/navigation'
 import ResultBanner from '@/components/shared/ResultBanner'
 import { MOCK_DATA } from '@/mocks/exam-variant-mocks'
-import type { NewQuestionModalValues } from '@/types/exam-variant'
+import type { NewQuestion } from '@/types/exam-variant'
 import NewQuestionLoading from './NewQuestionLoading'
 import NewQuestionHeader from './NewQuestionHeader'
 import NewQuestionAnswer from './NewQuestionAnswer'
@@ -16,14 +16,14 @@ export default function NewQuestionModal() {
   // 데이터 불러오는 상태 (현재 Mock 데이터를 사용하므로 set 함수 제외)
   const [loading] = useState(false)
   //   문제 불러오기 (현재 Mock 데이터를 사용하므로 set 함수 제외)
-  const [data] = useState<NewQuestionModalValues | null>(MOCK_DATA)
+  const [data] = useState<NewQuestion | null>(MOCK_DATA.data[0])
   //   문제 제출하기
   const [isSubmitted, setSubmitted] = useState<boolean>(false)
   //   문제 정답 상태
   const [isCorrect, setIsCorrect] = useState<boolean | null>(null)
   //   선택지 선택 상태
   const [selected, setIsSelected] = useState<number | null>(null)
-
+  const [frqAnswer, setFrqAnswer] = useState<number | null>(null)
   const router = useRouter()
 
   const handleClose = () => {
@@ -31,19 +31,29 @@ export default function NewQuestionModal() {
   }
 
   const handleSubmit = () => {
-    if (selected === null) {
-      return alert('문제를 선택해주세요!')
-    }
+    if (!data) return
+    const answer = data.type === 'MCQ' ? selected : frqAnswer
+    if (answer == null) return
+
     setSubmitted(true)
-    setIsCorrect(selected === data?.data.answer)
+    setIsCorrect(answer === data.answer)
   }
+
   // 원래 문제로 돌아가기
   const handleBack = () => {
     router.back()
   }
 
+  const handleChange = (answer: number) => {
+    setFrqAnswer(answer)
+  }
   return (
-    <Modal open={true} onClose={handleClose} className="md:max-w-145 lg:max-w-198">
+    <Modal
+      open={true}
+      onClose={handleClose}
+      className="md:max-w-145 md:max-h-168.5
+       lg:max-w-198 lg:max-h-204"
+    >
       {loading ? (
         <NewQuestionLoading />
       ) : (
@@ -54,7 +64,7 @@ export default function NewQuestionModal() {
             </ModalHeader>
             {/* 문제 */}
             <ModalContent className="flex flex-col gap-4 ">
-              <NewQuestionExam passage={data.data.passage} />
+              <NewQuestionExam passage={data.passage} />
               {/* 정오답 표시 배너 */}
               {isSubmitted && isCorrect !== null && (
                 <ResultBanner result={isCorrect ? 'correct' : 'wrong'} />
@@ -68,15 +78,18 @@ export default function NewQuestionModal() {
               <h3 className="font-bold text-neutral-600">답안 선택</h3>
               {/* 답안 선택지 */}
               <NewQuestionAnswer
-                options={data.data.options}
-                answer={data.data.answer}
+                type={data.type}
+                options={data.options ?? []}
+                answer={data.answer}
                 selected={selected}
+                frqAnswer={frqAnswer}
                 isSubmitted={isSubmitted}
                 onSelect={setIsSelected}
+                onFrqChange={handleChange}
               />
               {/* 오답 해설 */}
               <NewQuestionAnalysis
-                analysis={data.data.analysis}
+                analysis={data.analysis}
                 isSubmitted={isSubmitted}
                 isCorrect={isCorrect}
               />
@@ -85,8 +98,10 @@ export default function NewQuestionModal() {
             {/* 제출버튼 및 원래 페이지로 돌아가기 */}
             <ModalFooter>
               <NewQuestionAction
+                type={data.type}
                 isSubmitted={isSubmitted}
                 selected={selected}
+                frqAnswer={frqAnswer}
                 onSubmit={handleSubmit}
                 onBack={handleBack}
               />
