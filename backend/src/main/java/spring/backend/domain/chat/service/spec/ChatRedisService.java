80b61fbb39fd200d2d07a9b8a450b8693358b05f
mskim <msung98@naver.com>
2026-01-09 13:17:29 +0900

    feat: [BE] [G2-137]

--- backend/src/main/java/spring/backend/domain/chat/service/spec/ChatRedisService.java
+++ backend/src/main/java/spring/backend/domain/chat/service/spec/ChatRedisService.java
@@ -6,43 +6,21 @@ import spring.backend.domain.chat.dto.response.ChatMessageDto;
 
 public interface ChatRedisService {
 
-  /**
-   * 세션 초기화
-   *
-   * @param sessionId  세션 ID
-   * @param userId     사용자 ID
-   * @param questionId 질문 ID
-   */
-  void initializeSession(Long sessionId, UUID userId, Long questionId);
+  // 세션 초기화
+  void initializeSession(Long sessionId, UUID userId);
 
-  /**
-   * 메시지 저장
-   *
-   * @param sessionId 세션 ID
-   * @param message   메시지
-   */
+  // 메시지 저장
   void saveMessage(String sessionId, ChatMessageDto message);
 
-  /**
-   * 세션의 모든 메시지 조회
-   *
-   * @param sessionId 세션 ID
-   * @return 메시지 리스트
-   */
+  // 세션의 모든 메시지 조회
   List<ChatMessageDto> getSessionMessages(String sessionId);
 
-  /**
-   * 세션 소유자 ID 조회
-   *
-   * @param sessionId 세션 ID
-   * @return 사용자 ID
-   */
+  // 세션 소유자 ID 조회
   UUID getSessionOwnerId(String sessionId);
 
-  /**
-   * 세션 삭제
-   *
-   * @param sessionId 세션 ID
-   */
+  // 세션 삭제
   void deleteSession(String sessionId);
+
+  // 세션 권한 확인
+  boolean validateSessionOwner(Long sessionId, UUID userId);
 }
