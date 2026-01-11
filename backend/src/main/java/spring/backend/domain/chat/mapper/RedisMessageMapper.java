def6808ec3cb548681063fa928ee200c952eeaa8
mskim <msung98@naver.com>
2026-01-11 22:55:20 +0900

    feat: [BE] [G2-137]

--- backend/src/main/java/spring/backend/domain/chat/mapper/RedisMessageMapper.java
+++ backend/src/main/java/spring/backend/domain/chat/mapper/RedisMessageMapper.java
@@ -1,10 +1,15 @@
 package spring.backend.domain.chat.mapper;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
 import spring.backend.domain.chat.dto.enums.ChatRole;
 import spring.backend.domain.chat.dto.redis_dto.MessageDto;
 import spring.backend.domain.chat.dto.request.ChatSendRequest;
+import spring.backend.domain.chat.model.entity.ChatMessageContent;
 
+@Component
 public class RedisMessageMapper {
 
     public MessageDto toDtoUser(ChatSendRequest req) {
@@ -14,6 +19,20 @@ public class RedisMessageMapper {
                 .message(req.message())
                 .timestamp(LocalDateTime.now())
                 .build();
+    }
+
+    public ChatMessageContent toEntity(MessageDto dto) {
+        return ChatMessageContent.builder()
+                .role(dto.chatRole())
+                .content(dto.message())
+                .timestamp(dto.timestamp())
+                .build();
+    }
+    
+    public List<ChatMessageContent> toEntityList(List<MessageDto> dtoList) {
+        return dtoList.stream()
+                .map(this::toEntity)
+                .collect(Collectors.toList());
     }
 
 }
