02d8ad3d19cf4514814e370537e2e35995c43855
mskim <msung98@naver.com>
2026-01-11 22:05:23 +0900

    feat: [BE] [G2-137] dto 분리

---
+++ backend/src/main/java/spring/backend/domain/chat/dto/request/ChatSaveRequest.java
@@ -0,0 +1,14 @@
+package spring.backend.domain.chat.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record ChatSaveRequest(
+
+        @NotBlank(message = "sessionId 가 필요합니다.")
+        Long sessionId,
+
+        @NotBlank(message = "questionResultId 가 필요합니다.")
+        Long questionResultId
+
+) {
+}
