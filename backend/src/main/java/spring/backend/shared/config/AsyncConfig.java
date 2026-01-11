f86547d03e2dcf615c1fc7eea06544028668eb9a
mskim <msung98@naver.com>
2026-01-11 19:40:52 +0900

    feat: [BE] [G2-137] 비동기 전역 설정

---
+++ backend/src/main/java/spring/backend/shared/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package spring.backend.shared.config;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(50);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("async-chat-");
+        executor.initialize();
+        return executor;
+    }
+}
