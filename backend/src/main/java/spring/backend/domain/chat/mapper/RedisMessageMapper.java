f338f66ec70184f92604d666d0e6001137e436c5
mskim <msung98@naver.com>
2026-01-12 16:45:42 +0900

    feat: [BE] [G2-137] 메세지 큐용 맵퍼 메서드 추가

--- backend/src/main/java/spring/backend/domain/chat/mapper/RedisMessageMapper.java
+++ backend/src/main/java/spring/backend/domain/chat/mapper/RedisMessageMapper.java
@@ -5,31 +5,41 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 import spring.backend.domain.chat.dto.enums.ChatRole;
-import spring.backend.domain.chat.dto.redis_dto.MessageDto;
+import spring.backend.domain.chat.dto.redis_dto.RedisMessageDto;
 import spring.backend.domain.chat.dto.request.ChatSendRequest;
 import spring.backend.domain.chat.model.entity.ChatMessageContent;
 
 @Component
 public class RedisMessageMapper {
 
-    public MessageDto toDtoUser(ChatSendRequest req) {
+    // 사용자 메세지
+    public RedisMessageDto toDtoUser(ChatSendRequest req) {
 
-        return MessageDto.builder()
+        return RedisMessageDto.builder()
                 .chatRole(ChatRole.USER)
                 .message(req.message())
                 .timestamp(LocalDateTime.now())
                 .build();
     }
 
-    public ChatMessageContent toEntity(MessageDto dto) {
+    // LLM 메세지
+    public RedisMessageDto toDtoLlm(String llmResponse) {
+        return RedisMessageDto.builder()
+                .chatRole(ChatRole.LLM)
+                .message(llmResponse)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    public ChatMessageContent toEntity(RedisMessageDto dto) {
         return ChatMessageContent.builder()
                 .role(dto.chatRole())
                 .content(dto.message())
                 .timestamp(dto.timestamp())
                 .build();
     }
-    
-    public List<ChatMessageContent> toEntityList(List<MessageDto> dtoList) {
+
+    public List<ChatMessageContent> toEntityList(List<RedisMessageDto> dtoList) {
         return dtoList.stream()
                 .map(this::toEntity)
                 .collect(Collectors.toList());
