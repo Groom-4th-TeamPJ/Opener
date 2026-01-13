4d75aa4dfad0c2be983f55d1ad5657eff4c2ced2
JunwonLee <jununida@gmail.com>
2026-01-13 13:48:57 +0900

    feat: [BE] [G2-286] repository 추가

--- backend/src/main/java/spring/backend/domain/can/repository/impl/CanRepositoryImpl.java
+++ backend/src/main/java/spring/backend/domain/can/repository/impl/CanRepositoryImpl.java
@@ -1,4 +1,29 @@
 package spring.backend.domain.can.repository.impl;
 
-public class CanRepositoryImpl {
+import org.springframework.stereotype.Repository;
+import spring.backend.domain.can.model.entity.Can;
+import spring.backend.domain.can.repository.jpa.JpaCanRepository;
+import spring.backend.domain.can.repository.spec.CanRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class CanRepositoryImpl implements CanRepository {
+
+    private final JpaCanRepository jpaConRepository;
+
+    public CanRepositoryImpl(JpaCanRepository jpaConRepository) {
+        this.jpaConRepository = jpaConRepository;
+    }
+
+    @Override
+    public Optional<Can> findByUserId(UUID userId) {
+        return jpaConRepository.findByUserId(userId);
+    }
+
+    @Override
+    public Can save(Can can) {
+        return jpaConRepository.save(can);
+    }
 }
--- backend/src/main/java/spring/backend/domain/can/repository/jpa/JpaCanRepository.java
+++ backend/src/main/java/spring/backend/domain/can/repository/jpa/JpaCanRepository.java
@@ -1,4 +1,11 @@
 package spring.backend.domain.can.repository.jpa;
 
-public interface JpaCanRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import spring.backend.domain.can.model.entity.Can;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface JpaCanRepository extends JpaRepository<Can, Long> {
+    Optional<Can> findByUserId(UUID userId);
 }
