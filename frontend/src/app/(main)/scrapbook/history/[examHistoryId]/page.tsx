a6ead582ec0c5ad3012e191734158b41c011d548
Nahyun <from.owol@gmail.com>
2026-01-13 16:01:54 +0900

    feat: 스크랩북 상세조회 페이지 UI 구성

---
+++ frontend/src/app/(main)/scrapbook/history/[examHistoryId]/page.tsx
@@ -0,0 +1,9 @@
+import HistoryDetail from '@/components/scrapbook/HistoryDetail'
+
+export default function ExamHistory() {
+  return (
+    <div className="fixed inset-0 top-16 flex flex-col">
+      <HistoryDetail />
+    </div>
+  )
+}
