2c7498c3853dbe70a9db00112d55a52113ea80aa
Choijiin007 <cji2025@naver.com>
2026-01-14 10:34:33 +0900

    feat:[FE][G2-264] 스크랩북 리스트 UI 구성
    * feat:[FE] 축하 아이콘 추가
    
    * feat:[FE] 학습 종료 결과 UI 수정하여 추가
    
    * Remove:[FE] 사용하지 않는 아이콘 파일 삭제
    
    * refactor:[FE] 모바일 메뉴 패딩 수정
    
    * feat:[FE] 페이지네이션 아이콘 추가
    
    * feat:[FE] 페이지네이션 아이콘 추가
    
    * feat:[FE] 페이지네이션 훅
    
    * feat[FE] 스크랩북 리스트 구현 및 컴포넌트 분리
    
    * feat:[FE] 스크랩북 리스트 목데이터 및 타입 설정, 컴포넌트 경로 수정
    
    * Refactor:[FE] 스크랩북 목데이터 수정
    
    * remove:[FE] 폴더 이동으로 삭제
    
    * Rename:[FE] 폴더 위치 변경
    
    * Refactor:[FE] ApiSuccess<T> 타입 사용
    
    * feat:[FE] 스크랩북 리스트 연결
    
    * style:[FE] 리스트 아이템 세로길이 줄임, 각종 여백 수정
    
    ---------
    
    Co-authored-by: cji <gugugeul273@gmail.com>

---
+++ frontend/src/components/scrapbook/ScrapBookList.tsx
@@ -0,0 +1,63 @@
+'use client'
+import { Card, CardContent, CardHeader } from '@/components/common/Card'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { ScrapBookListMocks } from '@/mocks/scrapbook-list-mocks'
+import Image from 'next/image'
+import Button from '@/components/common/Button'
+import useScrapBookPagination from '@/hooks/scrapbook/use-scrap-book-pagination'
+import ScrapBookPagination from './ScrapBookPagination'
+import ScrapBookListItem from './ScrapBookListItem'
+
+export default function ScrapBookList() {
+  const router = useRouter()
+  const pageSize = 8
+  const totalCount = ScrapBookListMocks.data.examHistories.length
+  const search = useSearchParams()
+
+  const { currentPage, totalPages, previousPage, nextPage, startIndex, endIndex, pages } =
+    useScrapBookPagination({ totalCount, pageSize, search })
+  return (
+    <div className="flex flex-col w-full gap-4 min-h-dvh md:mt-6 lg:mt-10">
+      <div className="flex gap-4 items-center">
+        <Button
+          variant="ghost"
+          className="bg-none hover:bg-transparent"
+          onClick={() => router.push('/scrapbook')}
+        >
+          <Image src="/icons/chevron-left.svg" alt="뒤로 가기" width={24} height={24} />
+        </Button>
+        <h1 className="text-text-primary text-[23px] font-bold">
+          {ScrapBookListMocks.data.year}년 {ScrapBookListMocks.data.examType.name}
+        </h1>
+      </div>
+      <Card>
+        <CardHeader className="pb-0">
+          <div className="grid grid-cols-[1fr_4fr_1fr] gap-5 border-b border-b-neutral-600 p-3 pt-0">
+            <div>유형</div>
+            <div>문제</div>
+            <div>분석일시</div>
+          </div>
+        </CardHeader>
+
+        <CardContent className="pt-0">
+          {ScrapBookListMocks.data.examHistories.slice(startIndex, endIndex).map((history) => (
+            <ScrapBookListItem
+              key={history.examHistoryId}
+              id={history.examHistoryId}
+              categoryName={history.category.name}
+              passage={history.passage}
+              createdAt={history.createdAt}
+            />
+          ))}
+        </CardContent>
+      </Card>
+      <ScrapBookPagination
+        currentPage={currentPage}
+        previousPage={previousPage}
+        nextPage={nextPage}
+        totalPages={totalPages}
+        pages={pages}
+      />
+    </div>
+  )
+}
---
+++ frontend/src/components/scrapbook/ScrapBookListItem.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+type ScrapListItemProps = {
+  id: number
+  categoryName: string
+  passage: string
+  createdAt: string
+}
+export default function ScrapBookListItem({
+  id,
+  categoryName,
+  passage,
+  createdAt,
+}: ScrapListItemProps) {
+  return (
+    <Link href={`/scrapbook/${id}`}>
+      <div className="grid grid-cols-[1fr_4fr_1fr] gap-5 border-b border-b-neutral-200 px-3 h-15  items-center hover:bg-neutral-50">
+        <span className="truncate">{categoryName}</span>
+        <span className="truncate">{passage}</span>
+        <span className="truncate">{createdAt}</span>
+      </div>
+    </Link>
+  )
+}
