81e1fae7685a90b263eebadbeb88f46fc5820b50
mskim <msung98@naver.com>
2026-01-15 11:26:04 +0900

    feat: [BE] [G2-296] join 설정 변경

--- backend/src/main/java/spring/backend/domain/chat/model/entity/ChatMessage.java
+++ backend/src/main/java/spring/backend/domain/chat/model/entity/ChatMessage.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
@@ -28,7 +29,7 @@ public class ChatMessage extends BaseEntity {
     private Long id;
 
     @OneToOne
-    @Column(name = "question_result_id", nullable = false)
+    @JoinColumn(name = "question_result_id", nullable = false)
     private QuestionResult questionResult;
 
     @JdbcTypeCode(SqlTypes.JSON)
