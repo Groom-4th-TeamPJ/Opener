3b44f8f4fa6e9fd3fee227a2d944ab5a08ecfe6f
JunwonLee <jununida@gmail.com>
2026-01-16 23:07:35 +0900

    feat: [BE] [G2-316] 사용자가 오프너를 이용한 ExamResult 들의 ID로 오프너를 이용한 모든 문제 추출

--- backend/src/main/java/spring/backend/domain/exam/repository/spec/QuestionResultRepository.java
+++ backend/src/main/java/spring/backend/domain/exam/repository/spec/QuestionResultRepository.java
@@ -1,7 +1,10 @@
 package spring.backend.domain.exam.repository.spec;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import spring.backend.domain.exam.model.entity.QuestionResult;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QuestionResultRepository {
@@ -10,4 +13,6 @@ public interface QuestionResultRepository {
     QuestionResult save(QuestionResult questionResult);
 
     boolean existsByExamResultIdAndQuestionId(Long examResultId, Long questionId);
+
+    Page<QuestionResult> findAllByExamResultIdInAndIsOpenerIsTrue(List<Long> examResultIds, Pageable pageable);
 }
