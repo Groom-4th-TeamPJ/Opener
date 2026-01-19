1ea46652c6b0c025b3c678b53b024e831a3e7e55
cji <gugugeul273@gmail.com>
2026-01-20 01:24:19 +0900

    fix:[FE] 필드명 변경

--- frontend/src/components/scrapbook/ScrapBookListItem.tsx
+++ frontend/src/components/scrapbook/ScrapBookListItem.tsx
@@ -4,20 +4,20 @@ type ScrapListItemProps = {
   questionResultId: number
   categoryName: string
   passage: string
-  createdAt: string
+  openerUsedAt: string
 }
 export default function ScrapBookListItem({
   questionResultId,
   categoryName,
   passage,
-  createdAt,
+  openerUsedAt,
 }: ScrapListItemProps) {
   return (
     <Link href={`${ROUTES.SCRAPBOOK}/history/${questionResultId}`}>
       <div className="grid grid-cols-[1fr_4fr_1fr] gap-5 border-b border-b-neutral-200 px-3 h-15  items-center hover:bg-neutral-50">
         <span className="truncate">{categoryName}</span>
         <span className="truncate">{passage}</span>
-        <span className="truncate">{createdAt}</span>
+        <span className="truncate">{openerUsedAt}</span>
       </div>
     </Link>
   )
