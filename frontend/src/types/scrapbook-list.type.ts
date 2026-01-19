f3282bb394ccce327c906bab5ca0da8745eb21d3
cji <gugugeul273@gmail.com>
2026-01-20 03:06:35 +0900

    Refactor:[FE] 타입 수정

--- frontend/src/types/scrapbook-list.type.ts
+++ frontend/src/types/scrapbook-list.type.ts
@@ -1,15 +1,13 @@
 import { ApiSuccess } from './api.types'
+import { CodeName } from './exam'
 
 export type ScrapBookListData = {
   examYear: number
-  examType: { code: 'M06' | 'M09' | 'CSAT'; name: '6모' | '9모' | '수능' }
+  examType: CodeName
   questionResults: {
     content: {
       questionResultId: number
-      category: {
-        code: 'ALG' | 'GEO' | 'PROB' | 'CALC'
-        name: '수학I + 수학II' | '기하' | '확률' | '미적분'
-      }
+      category: CodeName
       questionNo: number
       passage: string
       openerUsedAt: string
