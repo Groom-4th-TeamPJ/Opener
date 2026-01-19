fbb2ee0d9759b0d9d773971004b6db1aeb18f821
Nahyun <from.owol@gmail.com>
2026-01-19 15:34:22 +0900

    refactor: 학습 결과 타입 수정

--- frontend/src/types/exam.ts
+++ frontend/src/types/exam.ts
@@ -81,10 +81,10 @@ export interface ScrapbookQuestion extends Question {
 }
 
 export type ResultData = {
-  solveTime: string
+  totalTimeSpent: string
   correctCount: number
-  wrongCount: number
-  openerCount: number
+  incorrectCount: number
+  openerUsageCount: number
 }
 
 export interface StopwatchRef {
