ec936d63328723677e88944186e4d3f9000816a9
tjdwns99 <beans2853@gmail.com>
2026-01-20 10:02:01 +0900

    Revert "fix: 날짜 포맷팅 적용"
    This reverts commit 2e3f2c6b5bceef9cc506b4d8078bf6287ec26c56.

--- frontend/src/types/scrapbook-list.type.ts
+++ frontend/src/types/scrapbook-list.type.ts
@@ -11,22 +11,17 @@ export interface ScrapbookData {
 
 export type ScrapBookListData = {
   examYear: number
-  examType: CodeName
+  examType: { code: 'M06' | 'M09' | 'CSAT'; name: '6모' | '9모' | '수능' }
   questionResults: {
-    content: {
-      questionResultId: number
-      category: CodeName
-      questionNo: number
-      passage: string
-      openerUsedAt: string
-    }[]
-
-    page: number
-    size: number
-    totalElements: number
-    totalPages: number
-    last: boolean
-  }
+    questionResultId: number
+    category: {
+      code: 'ALG' | 'GEO' | 'PROB' | 'CALC'
+      name: '수학I + 수학II' | '기하' | '확률' | '미적분'
+    }
+    questionNo: number
+    passage: string
+    createdAt: string
+  }[]
 }
 
 export type ScrapBookListResponse = ApiSuccess<ScrapBookListData>
