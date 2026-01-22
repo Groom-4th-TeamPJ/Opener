713a9f143c703677bb4cef1ffa1a8a074f1d8dee
Choijiin007 <cji2025@naver.com>
2026-01-22 10:54:52 +0900

    fix:[G2-369][FE] 스크랩북 리스트 수식 렌더링 수정
    feat:[FE] displayMode:false로 블록수식 처리
    
    Co-authored-by: cji <gugugeul273@gmail.com>

--- frontend/src/utils/render-latex.ts
+++ frontend/src/utils/render-latex.ts
@@ -1,11 +1,19 @@
 import katex from 'katex'
 
-export default function renderLatex(text: string): string {
+type RenderLatexOptions = {
+  blockDisplayMode?: boolean
+}
+export default function renderLatex(text: string, option: RenderLatexOptions = {}): string {
+  const { blockDisplayMode = true } = option
+
   return (
     text
       // 블록 수식 처리 (\[...\])
       .replace(/\\\[([\s\S]+?)\\\]/g, (_, latex) => {
-        return katex.renderToString(latex.trim(), { displayMode: true, throwOnError: false })
+        return katex.renderToString(latex.trim(), {
+          displayMode: blockDisplayMode,
+          throwOnError: false,
+        })
       })
 
       // 인라인 수식 처리 (\(...\))
